6e565f98767e340dac8bad4df8813d17581441c7
Cristobal Galleguillos Hart <cristobalgh@gmail.com>
2019-06-17 16:37:44 -0400

    hazlo mejor

--- application.py
+++ application.py
@@ -8,3 +8,6 @@ def index():
     now = datetime.datetime.now()
     new_year = now.month == 12 and now.day==27
     return render_template("index.html", new_year=new_year)
+
+if __name__== "__main__":
+    app.run()
