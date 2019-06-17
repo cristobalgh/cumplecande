300abf17bb3ec9940bea4caea909e9968f2c0fa5
Cristobal Galleguillos Hart <cristobalgh@gmail.com>
2019-06-17 15:54:27 -0400

    cristobalgh/chrona  witt  fosddjfpsd  sdfsd
    a
    
    :qw
    '#' will be ignored, and an empty message aborts the commit.

---
+++ application.py
@@ -0,0 +1,10 @@
+import datetime
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    new_year = now.month == 12 and now.day==27
+    return render_template("index.html", new_year=new_year)
