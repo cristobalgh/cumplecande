02f115e956f332a5812963615f0a03a8cb439a61
Cristobal Galleguillos Hart <cristobalgh@gmail.com>
2019-06-18 13:27:57 -0400

    hola

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    now = datetime.datetime.now()
+    si_cumple = now.month==12 and now.day==27
+    return render_template("index.html", si_cumple=si_cumple)
+
+if __name__=="__main__":
+    app.run()
