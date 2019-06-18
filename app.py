04b8e1b9d8d7364d990e9d05950d07027cc522d4
Cristobal Galleguillos Hart <cristobalgh@gmail.com>
2019-06-18 10:31:40 -0400

    hazlo mejor

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.datetime.now()
-    si_cumple = now.month == 12 and now.day==27
+    si_cumple = now.month==6 and now.day==18
     return render_template("index.html", si_cumple=si_cumple)
 
 if __name__=="__main__":
