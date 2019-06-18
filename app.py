e45b3162711e462419b1752fc0d39754322e3d13
Cristobal Galleguillos Hart <cristobalgh@gmail.com>
2019-06-18 08:59:08 -0400

    jaja

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
+    now = datetime.datetime.now()
+    new_year = now.month == 12 and now.day==27
     return 'Si! esta funcionando aún.'
 
 if __name__=="__main__":
